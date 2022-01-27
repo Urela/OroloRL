e8f1d01b9034b4e4cd49b4707f577d68ad8bd7aa
Orula <93521654+Orula@users.noreply.github.com>
2022-01-27 21:30:10 +0200

    Add files via upload

--- ppo.py
+++ ppo.py
@@ -1,3 +1,6 @@
+"""
+Continous PPO
+"""
 import gym
 import random
 import pybullet_envs
@@ -71,10 +74,10 @@ class PPO:
     idx = self.idx % self.size
     self.idx += 1
     self.states  [idx] = state
-    self.actions [idx] = action
+    self.actions [idx] = action .detach().numpy()
     self.rewards [idx] = reward
-    self.probs   [idx] = probs
-    self.values  [idx] = vals
+    self.probs   [idx] = probs  .detach().numpy()
+    self.values  [idx] = vals   .detach().numpy()
     self.dones   [idx] = 1-(done)
 
   def get_action(self, obs):
@@ -86,9 +89,10 @@ class PPO:
     dist = Normal(mean, std)
     action = dist.sample()
 
-    probs  = torch.squeeze(dist.log_prob(action)).detach().numpy().sum(1)
-    action = torch.squeeze(action).detach().numpy()
-    value  = torch.squeeze(value).detach().numpy()
+    probs  = torch.squeeze(dist.log_prob(action)).sum(1)
+    action = torch.squeeze(action)
+    value  = torch.squeeze(value)
+    #print( action.shape, probs.shape, value.shape)
     return action, probs, value
 
   def train(self):
@@ -115,19 +119,26 @@ class PPO:
       lrnow = frac * self.lr
       self.AC.optimizer.param_groups[0]["lr"] = lrnow
 
+      c, h, w = self.states.shape
+      _states = self.states.reshape(c*h, w)
+      _actions = self.actions.reshape(-1)
+      _probs = self.probs.reshape(-1)
+
       for batch in batches:
-        states    = torch.tensor(self.states[batch], dtype=torch.float).to('cpu')
-        actions   = torch.tensor(self.actions[batch], dtype=torch.float).to('cpu')
-        old_probs = torch.tensor(self.probs[batch], dtype=torch.float).to('cpu')
+        states    = torch.tensor(_states[batch], dtype=torch.float).to('cpu')
+        actions   = torch.tensor(_actions[batch], dtype=torch.float).to('cpu')
+        old_probs = torch.tensor(_probs[batch], dtype=torch.float).to('cpu')
 
         # Evaluating old actions and values
-
+        #print(states.shape)
         action, new_probs, crit = self.get_action(states)
         new_probs = torch.tensor( new_probs ).to('cpu')
-        print( new_probs.shape, old_probs.shape)
+        #print( _states.shape, states.shape, new_probs.shape, old_probs.shape)
+
         # Finding the ratio (pi_theta / pi_theta__old)
         #new_probs = dist.log_prob(actions)
         ratio = new_probs.exp() / old_probs.exp()
+        #print(ratio)
 
         # Finding Surrogate Loss
         surr1 = ratio * advantage[batch]
@@ -136,13 +147,13 @@ class PPO:
 
         # final loss of clipped objective PPO: loss = actor_loss + 0.5*critic_loss 
         loss = -torch.min(surr1, surr2).mean() + 0.5*((returns-crit)**2).mean()
+        #print( loss) 
 
         # take gradient step
         self.AC.optimizer.zero_grad()
-        loss.mean().backward()
-        nn.utils.clip_grad_norm_(self.AC.parameters(), 0.5)
+        loss.backward()
+        #nn.utils.clip_grad_norm_(self.AC.parameters(), 0.5)
         self.AC.optimizer.step()
-
 
     self.idx=0
     pass
@@ -152,21 +163,20 @@ update_frequency = 128
 num_envs  = 4       # number of parallel environments
 env = gym.vector.SyncVectorEnv(
     [lambda: gym.make('HopperBulletEnv-v0') for _ in range(num_envs) ]
+    #[lambda: gym.make('CartPole-v1') for _ in range(num_envs) ]
 )
 env = gym.wrappers.RecordEpisodeStatistics(env)
 
-#agent = PPO( env.observation_space, env.action_space )
-#agent = PPO(env.single_observation_space, env.single_action_space, )
 agent = PPO(env.single_observation_space, env.single_action_space, batch_size=4, num_steps=update_frequency, num_envs=num_envs)
 
-#env.render(mode='human')
-#env.render() scores = []
+#env.render() 
 scores = []
-for epi in range(1000):
-  obs = env.reset()
-  while True:
+obs = env.reset()
+for epi in range(3000):
+  DONE = False
+  while not DONE:
 
-    action, probs, vals = agent.get_action( torch.from_numpy(obs).float().to('cpu') )
+    action, probs, vals = agent.get_action( torch.tensor([obs]).float().to('cpu') )
     #action, probs, vals = env.action_space.sample(), 0 ,0
     _obs, reward, done, info = env.step(action)
     agent.store(obs, action, reward, probs, vals, done)
@@ -180,5 +190,7 @@ for epi in range(1000):
       if "episode" in item.keys():
         scores.append(int(item['episode']['r']))
         avg_score = np.mean(scores[-100:]) # moving average of last 100 episodes
-        print(f"Episode {epi}, Return: {scores[-1]}, Avg return: {avg_score}")
-        break
+        #print(f"Episode {epi}, Return: {scores[-1]}, Avg return: {avg_score}")
+        if epi % 10 ==0:
+          print(f"global_step={time_step}, episode_reward={scores[-1]}, Avg return: {avg_score}")
+        DONE = True
